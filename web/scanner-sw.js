aaba7d2bd5dc078d94d1aecd4801d6419306b268
Greenl1s <136644511+Greenl1s@users.noreply.github.com>
2026-09-21 00:47:42 +0300

    Add files via upload

--- web/scanner-sw.js
+++ web/scanner-sw.js
@@ -1,4 +1,4 @@
-const CACHE='instrument-scanner-v1';
+const CACHE='instrument-scanner-v2';
 const SHELL=['./scanner.html','./scanner.css','./js/scanner.js','./js/image-fingerprint.js'];
 self.addEventListener('install',(event)=>event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(SHELL))));
 self.addEventListener('activate',(event)=>event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE).map((key)=>caches.delete(key))))));
